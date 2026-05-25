3e0f80d0495ff417c9495eb236e3bc57a22d9c27
Umar Atimomo <umar.atimomo@gmail.com>
2026-05-25 20:21:35 +0300

    took out extra (') in index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const core = require('@actions/core');
 
 try {
-    const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello', ${nameToGreet}!`);
-    core.setOutput('time', new Date().toTimeString());
+  const nameToGreet = core.getInput('who-to-greet');
+  console.log(`Hello, ${nameToGreet}!`);
+  core.setOutput('time', new Date().toTimeString());
 } catch (error) {
-    core.setFailed(error.message);
+  core.setFailed(error.message);
 }
 
 
